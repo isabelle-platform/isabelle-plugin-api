dd2ba89350766188666a1ba777bd65bdb81acb02
Maxim Menshikov <maxim@menshikov.org>
2024-04-07 23:58:03 +0100

    Add logging to plugin pool

--- src/plugin_pool.rs
+++ src/plugin_pool.rs
@@ -1,6 +1,7 @@
 use crate::api::*;
 use libloading::{Library, Symbol};
 use std::fs;
+use log::info;
 
 pub struct PluginPool {
 }
@@ -10,10 +11,12 @@ impl PluginPool {
 
     pub fn load_plugins(&self, api: &PluginApi, path: &str) {
         let paths = fs::read_dir(path).unwrap();
-
+        info!("Loading plugins from {}", path);
         for path in paths {
             let file_name = path.unwrap().path().display().to_string();
+            info!("File name: {}", file_name);
             if file_name.starts_with("libisabelle-plugin-") {
+                info!("Library: {}", file_name);
                 unsafe {
                     let lib = Library::new(file_name).unwrap();
                     let func: Symbol<IsabellePluginRegisterFn> = lib.get(b"register").unwrap();
