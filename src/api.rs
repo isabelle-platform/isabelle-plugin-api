c11d61695a8b2127551af4a07f1a9f4c4c5ce0c9
Maxim Menshikov <maxim@menshikov.org>
2024-04-07 19:03:09 +0100

    Update API

--- src/api.rs
+++ src/api.rs
@@ -1,5 +1,15 @@
+use std::collections::HashMap;
 use isabelle_dm::data_model::item::Item;
 use isabelle_dm::data_model::list_result::ListResult;
+use isabelle_dm::data_model::process_result::ProcessResult;
+
+pub enum WebResponse {
+	Ok,
+	NotFound,
+	Unauthorized,
+	BadRequest,
+	Forbidden,
+}
 
 /* database */
 type IsabelleDbGetAllItemsFn = fn(
@@ -34,6 +44,79 @@ type IsabelleFnSendEmail = fn(
 	subject: &str,
 	body: &str);
 
+/* route API */
+type IsabelleRouteItemPreEditHook = fn(
+    user: &Option<Item>,
+    collection: &str,
+    old_itm: Option<Item>,
+    itm: &mut Item,
+    del: bool,
+    merge: bool) -> ProcessResult;
+type IsabelleRouteRegisterItemPreEditHook = fn(
+	name: &str,
+	hook: IsabelleRouteItemPreEditHook) -> bool;
+
+type IsabelleRouteItemPostEditHook = fn(
+    collection: &str,
+    id: u64,
+    del: bool);
+type IsabelleRouteRegisterItemPostEditHook = fn(
+	name: &str,
+	hook: IsabelleRouteItemPostEditHook) -> bool;
+
+type IsabelleRouteItemAuthHook = fn(
+	user: &Option<Item>,
+    collection: &str,
+    id: u64,
+    new_item: Option<Item>,
+    del: bool) -> bool;
+type IsabelleRouteRegisterItemAuthHook = fn(
+	name: &str,
+	hook: IsabelleRouteItemAuthHook) -> bool;
+
+type IsabelleRouteItemListFilterHook = fn(
+	user: &Option<Item>,
+    collection: &str,
+    context: &str,
+    map: &mut HashMap<u64, Item>);
+type IsabelleRouteRegisterItemListFilterHook = fn(
+	name: &str,
+	hook: IsabelleRouteItemListFilterHook) -> bool;
+
+type IsabelleRouteUrlHook = fn(
+	user: &Option<Item>,
+	query: &str) -> WebResponse;
+type IsabelleRouteRegisterUrlHook = fn(
+	name: &str,
+	hook: IsabelleRouteUrlHook);
+
+type IsabelleRouteUnprotectedUrlHook = fn(
+	user: &Option<Item>,
+	query: &str) -> WebResponse;
+type IsabelleRouteRegisterUnprotectedUrlHook = fn(
+	name: &str,
+	hook: IsabelleRouteUnprotectedUrlHook);
+
+type IsabelleRouteUnprotectedUrlPostHook = fn(
+	user: &Option<Item>,
+	query: &str,
+	itm: &Item) -> WebResponse;
+type IsabelleRouteRegisterUnprotectedUrlPostHook = fn(
+	name: &str,
+	hook: IsabelleRouteUnprotectedUrlPostHook);
+
+type IsabelleRouteCollectionReadHook = fn(
+	collection: &str,
+	itm: &mut Item) -> bool;
+type IsabelleRouteRegisterCollectionReadHook = fn(
+	name: &str,
+	hook: IsabelleRouteCollectionReadHook);
+
+type IsabelleRouteCallOtpHook = fn(
+	itm: &Item);
+type IsabelleRouteRegisterCallOtpHook = fn(
+	name: &str,
+	hook: IsabelleRouteCallOtpHook);
 
 pub struct PluginApi {
 	/* database */
@@ -48,5 +131,17 @@ pub struct PluginApi {
 
 	/* exposed functions */
 	pub fn_send_email: IsabelleFnSendEmail,
+
+	/* routes */
+	pub route_register_item_pre_edit_hook: IsabelleRouteRegisterItemPreEditHook,
+	pub route_register_item_post_edit_hook: IsabelleRouteRegisterItemPostEditHook,
+	pub route_register_item_auth_hook: IsabelleRouteRegisterItemAuthHook,
+	pub route_register_item_list_filter_hook: IsabelleRouteRegisterItemListFilterHook,
+	pub route_register_url_hook: IsabelleRouteRegisterUrlHook,
+	pub route_register_unprotected_url_hook: IsabelleRouteRegisterUnprotectedUrlHook,
+	pub route_register_unprotected_url_post_hook: IsabelleRouteRegisterUnprotectedUrlPostHook,
+
+	pub route_register_collection_read_hook: IsabelleRouteRegisterCollectionReadHook,
+	pub route_register_call_otp_hook: IsabelleRouteRegisterCallOtpHook,
 }
 
