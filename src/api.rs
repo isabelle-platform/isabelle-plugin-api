5c7d6f13ebcd6c86b0a666ee0eea05fe0db263e7
Maxim Menshikov <maxim@menshikov.org>
2024-04-10 02:15:28 +0100

    API: add verify_password [2]

--- src/api.rs
+++ src/api.rs
@@ -79,7 +79,7 @@ pub struct PluginApi {
     pub auth_check_role: Box<dyn Fn(&Option<Item>, &str) -> bool>,
     pub auth_get_new_salt: Box<dyn Fn() -> String>,
     pub auth_get_password_hash: Box<dyn Fn(&str, &str) -> String>,
-    pub auth_verify_pasword: Box<dyn Fn(&str, &str) -> bool>,
+    pub auth_verify_password: Box<dyn Fn(&str, &str) -> bool>,
 
     /* exposed functions */
     pub fn_send_email: Box<dyn Fn(&str, &str, &str)>,
@@ -151,7 +151,7 @@ impl PluginApi {
                 return "".to_string();
             }),
 
-            auth_verify_pasword: Box::new(|_pw, _hash| {
+            auth_verify_password: Box::new(|_pw, _hash| {
                 return false;
             }),
 
