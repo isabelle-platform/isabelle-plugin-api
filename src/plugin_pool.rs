86d0cb4c683ce14dc06a70fde3c6fe6f0fde405f
Maxim Menshikov <maxim@menshikov.org>
2024-04-10 01:45:07 +0100

    Plugin Pool: use full library paths

--- src/plugin_pool.rs
+++ src/plugin_pool.rs
@@ -13,12 +13,13 @@ impl PluginPool {
         let paths = fs::read_dir(path).unwrap();
         info!("Loading plugins from {}", path);
         for path in paths {
-            let file_name = path.unwrap().path().file_name().unwrap().to_string_lossy().to_string();
+            let file_name = path.as_ref().unwrap().path().file_name().unwrap().to_string_lossy().to_string();
             info!("File name: {}", file_name);
             if file_name.starts_with("libisabelle_plugin_") {
                 info!("Library: {}", file_name);
                 unsafe {
-                    info!("Loading library");
+                    let full = path.as_ref().unwrap().path().canonicalize().unwrap().to_string_lossy().to_string();
+                    info!("Loading library {}", full);
                     match Library::new(file_name.clone()) {
                         Ok(lib) => {
                             info!("Library loaded");
