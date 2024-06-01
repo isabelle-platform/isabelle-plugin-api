a93ef2d147e87457679f0f7e42d2c78bda74327f
Maxim Menshikov <maxim@menshikov.org>
2024-06-01 20:08:37 +0100

    Update documentation

--- src/api.rs
+++ src/api.rs
@@ -1,8 +1,32 @@
+/*
+ * Isabelle project
+ *
+ * Copyright 2024 Maxim Menshikov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 use std::collections::HashMap;
 use isabelle_dm::data_model::item::Item;
 use isabelle_dm::data_model::list_result::ListResult;
 use isabelle_dm::data_model::process_result::ProcessResult;
 
+/// Canonical web responses
 pub enum WebResponse {
     Ok,
     OkData(String),
@@ -65,6 +89,7 @@ pub type IsabelleRouteCallOtpHook = fn(
     api: &PluginApi,
     itm: &Item);
 
+/// Generic plugin API structure
 pub struct PluginApi {
     /* database */
     pub db_get_all_items: Box<dyn Fn(&str, &str, &str) -> ListResult>,
@@ -118,6 +143,8 @@ pub struct PluginApi {
 }
 
 impl PluginApi {
+
+    /// Fill API with empty implementation
     pub fn new() -> Self {
         Self {
             /* database */
